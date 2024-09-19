b6f572d1af87a5961033c904de8e37aebc67749c
In Ho Seo <seoluke7203@hotmail.com>
2024-09-19 17:45:46 +0900

    Initial Commit

--- src/Pages/Landing/LandingPage.jsx
+++ src/Pages/Landing/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 export default function LandingPage() {
   return (
     <div>
-      <h1>Welcome to Landing Page!</h1>
+      <h1 className='text-3xl font-bold underline'>Welcome to Landing Page!</h1>
+      <Button>Here</Button>
     </div>
   )
 }
