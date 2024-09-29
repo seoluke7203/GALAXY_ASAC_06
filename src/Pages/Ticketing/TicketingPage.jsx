846225e87067c280693979501fc5e23219960e77
홍혁진 <gurwls123164@naver.com>
2024-09-29 13:26:09 +0900

    공연정보 작업물 정리

--- src/Pages/Ticketing/TicketingPage.jsx
+++ src/Pages/Ticketing/TicketingPage.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import InfoMain from '../../components/ui/performaceInfo/infoMain'
-import Menu from '../../components/ui/performaceMenu/Menu'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../components/ui/tabs"
+import useEventInfo from '../../custom/useEventInfo'
+
 
 export default function TicketingPage() {
+  const posterInfo = useEventInfo();
   return (
     <div>
       <InfoMain/>
-      <Menu/>
+      <Tabs defaultValue="performaceInfo" className='bg-white'>
+        <TabsList>
+          <TabsTrigger value="performaceInfo" >공연정보</TabsTrigger>
+          <TabsTrigger value="review">관람후기</TabsTrigger>
+          <TabsTrigger value="qna">Q&A</TabsTrigger>
+        </TabsList>
+        <TabsContent value="performaceInfo"> <PosterInfo src={posterInfo}/> </TabsContent>
+        <TabsContent value="review"> 관람후기 게시판 </TabsContent>
+        <TabsContent value="qna"> qna </TabsContent>
+      </Tabs>
+
     </div>
   )
 }
+
+const PosterInfo = ({ src }) => (
+  <div className='posterinfo'>
+    <img src={src} />
+  </div>
+)
--- src/custom/useEventInfo.js
+++ src/custom/useEventInfo.js
@@ -1,34 +1,29 @@
-import posterPicture from '../galaxyImage.jpeg';
+import posterPicture from '../galaxyImage.jpeg'
+import posterINFO from '../jesusMainPic.jpg'
+
 
 const useEventInfo = () => {
-    const eventTitle = "2024 뮤지컬 <지저스 크라이스트 슈퍼스타>";
-    const eventRank = "뮤지컬 주간 17위";
-    const posterSrc = posterPicture;
-  
-    const eventDetails = [
-      { label: "장소", value: "광림아트센터 BBCH홀" },
-      { label: "공연기간", value: "2024.11.07 ~ 2025.01.12" },
-      { label: "공연시간", value: "135분 (인터미션 20분 포함)" },
-      { label: "관람연령", value: "미취학아동입장불가" }
-    ];
+  const eventTitle = '2024 뮤지컬 <지저스 크라이스트 슈퍼스타>'
+  const eventRank = '뮤지컬 주간 17위'
+  const posterSrc = posterPicture
+  const posterInfo = posterINFO
 
-    const ticketInfo = [
-      "VIP석 150,000원",
-      "R석 120,000원",
-      "S석 90,000원",
-      "A석 60,000원"
-    ];
-  
-    
-  
-    return {
-      eventTitle,
-      eventRank,
-      posterSrc,
-      eventDetails,
-      ticketInfo,
-    };
-  };
-  
-  export default useEventInfo;
-  
+  const eventDetails = [
+    { label: '장소', value: '광림아트센터 BBCH홀' },
+    { label: '공연기간', value: '2024.11.07 ~ 2025.01.12' },
+    { label: '공연시간', value: '135분 (인터미션 20분 포함)' },
+    { label: '관람연령', value: '미취학아동입장불가' },
+  ]
+
+  const ticketInfo = ['VIP석 150,000원', 'R석 120,000원', 'S석 90,000원', 'A석 60,000원']
+
+  return {
+    eventTitle,
+    eventRank,
+    posterSrc,
+    eventDetails,
+    ticketInfo,
+  }
+}
+
+export default useEventInfo
