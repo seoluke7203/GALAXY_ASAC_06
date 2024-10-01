f2b31a03b89aab8c263ac055158947341e2f9a55
In Ho Seo <seoluke7203@hotmail.com>
2024-10-01 21:59:32 +0900

    Merge 오류 해결

--- src/Pages/Ticketing/TicketingPage.jsx
+++ src/Pages/Ticketing/TicketingPage.jsx
@@ -8,24 +8,24 @@ export default function TicketingPage() {
   const posterInfo = useEventInfo()
   return (
     <div className='w-full flex flex-row items-center justify-center'>
-    <div className='w-2/3'>
-      <InfoMain />
-      <Tabs defaultValue='performaceInfo' >
-        <TabsList className=''>
-          <TabsTrigger value='performaceInfo'>공연정보</TabsTrigger>
-          <TabsTrigger value='review'>관람후기</TabsTrigger>
-          <TabsTrigger value='qna'>Q&A</TabsTrigger>
-        </TabsList>
-        <TabsContent value='performaceInfo'>
-          <PosterInfo src='./src/assets/jesusMainPic.jpg' />
-        </TabsContent>
-        <TabsContent value='review'> 관람후기 게시판 </TabsContent>
-        <TabsContent value='qna'> qna </TabsContent>
-      </Tabs>
-    </div>
-    <div>
-      <DateSelectUI/>
-    </div>
+      <div className='w-2/3'>
+        <InfoMain />
+        <Tabs defaultValue='performaceInfo'>
+          <TabsList className=''>
+            <TabsTrigger value='performaceInfo'>공연정보</TabsTrigger>
+            <TabsTrigger value='review'>관람후기</TabsTrigger>
+            <TabsTrigger value='qna'>Q&A</TabsTrigger>
+          </TabsList>
+          <TabsContent value='performaceInfo'>
+            <PosterInfo src='./src/assets/jesusMainPic.jpg' />
+          </TabsContent>
+          <TabsContent value='review'> 관람후기 게시판 </TabsContent>
+          <TabsContent value='qna'> qna </TabsContent>
+        </Tabs>
+      </div>
+      <div>
+        <DateSelectUI />
+      </div>
     </div>
   )
 }
