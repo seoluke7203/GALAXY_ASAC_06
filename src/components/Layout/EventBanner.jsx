06ae6a550153a94de356029ce510779008e75745
coos02 <cosuis@naver.com>
2024-10-04 17:55:08 +0900

    결제 완료 페이지 UI & 이벤트 배너 스타일 수정

--- src/components/Layout/EventBanner.jsx
+++ src/components/Layout/EventBanner.jsx
@@ -1,20 +1,22 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 const EventComponent = () => {
-    return (
-      <div className='flex justify-center mb-10'>
-        <Link to ='/'>
-        <img className='w-[380px] h-auto m-3' src='../src/assets/event1.png'/>
+  return (
+    <div className='my-16'>
+      <h2 className='text-3xl font-bold underline'>Event</h2>
+      <div className='flex gap-5 justify-center mt-10'>
+        <Link to='/'>
+          <img className='h-auto' src='../src/assets/event1.png' />
         </Link>
-        <Link to ='/'>
-        <img className='w-[380px] h-auto m-3' src='../src/assets/event2.png'/>
+        <Link to='/'>
+          <img className='h-auto' src='../src/assets/event2.png' />
         </Link>
-        <Link to ='/'>
-        <img className='w-[380px] h-auto m-3' src='../src/assets/event3.png'/>
+        <Link to='/'>
+          <img className='h-auto' src='../src/assets/event3.png' />
         </Link>
       </div>
-    )
-  }
-  
-  export default EventComponent
-  
+    </div>
+  )
+}
+
+export default EventComponent
