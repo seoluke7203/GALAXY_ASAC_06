4ec8bd2933d7ff8a9269d317022ee7c2df9e2056
coos02 <cosuis@naver.com>
2024-11-21 13:22:59 +0900

    feat : sub Banner & 로그인(미완)

--- src/Pages/Landing/LandingPage.jsx
+++ src/Pages/Landing/LandingPage.jsx
@@ -5,41 +5,6 @@ import SubBanner from '@/components/LandingComponent/SubBanner'
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router'
 
-const subBannerContents = [
-  {
-    id: 1,
-    title: 'TEST',
-    genre: '뮤지컬',
-    content: '내용1',
-    link: '/ticketing',
-    productSrc: '../src/assets/images/musicalTestImg.gif',
-  },
-  {
-    id: 2,
-    title: 'TEST2',
-    genre: '뮤지컬',
-    content: '내용2',
-    link: '/ticketing',
-    productSrc: '../src/assets/images/musicalTestImg.gif',
-  },
-  {
-    id: 3,
-    title: 'TEST3',
-    genre: '뮤지컬',
-    content: '내용3',
-    link: '/ticketing',
-    productSrc: '../src/assets/images/concertTestImg.gif',
-  },
-  {
-    id: 4,
-    title: 'TEST4',
-    genre: '뮤지컬',
-    content: '내용4',
-    link: '/ticketing',
-    productSrc: '../src/assets/images/concertTestImg.gif',
-  },
-]
-
 export default function LandingPage() {
   const navigate = useNavigate()
   // const location = useLocation()
@@ -72,7 +37,7 @@ export default function LandingPage() {
   }
 
   const [mainBannerData, setMainBannerData] = useState([]) // 더미 데이터 세팅. 추후 변경
-  const [subBannerData, setSubBannerData] = useState(subBannerContents) // 더미 데이터 세팅. 추후 변경
+  const [subBannerData, setSubBannerData] = useState([]) // 더미 데이터 세팅. 추후 변경
 
   // useEffect -> genre -> MainBanner 데이터 호출 (max : 10)
   // setMainBanner
@@ -86,16 +51,30 @@ export default function LandingPage() {
         method: 'GET',
         headers: { 'Content-Type': 'application.json' },
       }
-      fetch(`http://localhost:8080/products/${genre}`, requestOptions)
+      fetch(`http://localhost:8080/api/products/main/${genre}`, requestOptions)
         .then((res) => res.json())
-        .then((data) => {
-          console.log('data', data)
-          setMainBannerData(data.data)
+        .then((result) => {
+          setMainBannerData(result.data)
+        })
+    }, [genre])
+  }
+
+  const fetchSubBannerData = () => {
+    useEffect(() => {
+      const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application.json' },
+      }
+      fetch(`http://localhost:8080/api/products/sub/${genre}`, requestOptions)
+        .then((res) => res.json())
+        .then((result) => {
+          setSubBannerData(result.data)
         })
     }, [genre])
   }
 
   fetchMainBannerData()
+  fetchSubBannerData()
 
   return (
     <div className='main'>
